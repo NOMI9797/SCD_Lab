68961d5f08a0852c36d3cc0265d14d7e8b0f6162
Noman Ahmed <139885182+NOMI9797@users.noreply.github.com>
2025-03-11 09:25:35 +0500

    new branch push

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,14 @@
- 
+import SignUp from './pages/SignUp'
+import Login from './pages/Login'
+
+function App() {
+  return (
+    <div>
+      {/* For now, we'll show the Login page. Later we can add routing */}
+      <Login />
+      {/* <SignUp /> */}
+    </div>
+  )
+}
+
+export default App 
